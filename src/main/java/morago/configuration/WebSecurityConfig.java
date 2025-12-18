de190d9ed5c8d0f07c713dd858bc8c62f35ea545
Ilyosjon <the.mechanic99@bk.ru>
2025-12-18 10:26:17 +0900

    enabled method level security

--- src/main/java/morago/configuration/WebSecurityConfig.java
+++ src/main/java/morago/configuration/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,6 +29,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig  {
     private final CustomUserDetailService customUserDetailService;
     private final JwtFilter jwtFilter;
